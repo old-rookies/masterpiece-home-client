1b2312a92640a27c1ab08c4b4c5eaf03a7f8d687
Yoo KyeongSoo <dev.yoogomja@gmail.com>
2019-07-09 17:24:38 +0900

    commit for checkout
    Signed-off-by: Yoo KyeongSoo <dev.yoogomja@gmail.com>

--- src/common/nav/Nav.js
+++ src/common/nav/Nav.js
@@ -6,14 +6,17 @@ import "./Nav.scss";
 
 // 공통 사용될 Nav 함수
 class Nav extends React.Component {
+    // override
+    // function render(){ /* CODE */ return(REACT+HTML); }
     render() {
+        // 자바스크립트 시작
         let navItems = [
             { url: "service", text: "Service", active: true },
             { url: "team", text: "Team", active: false },
             { url: "press", text: "Press", active: false },
             { url: "contact", text: "Contact", active: false }
         ];
-
+        // 자바스크립트 끝
         return (
             <nav className="navbar navbar-expand-lg navbar-light">
                 <NavLink className="navbar-brand" exact to="/">
@@ -39,6 +42,8 @@ class Nav extends React.Component {
                         {navItems.map((value, index) => {
                             return (
                                 <NavItem
+                                //  반복되는 요소를 구분짓기 위해서 기본으로 필요한 값
+                                //  여기서 index는 배열에서 value가 몇번째 요소인지 나타내는 값
                                     key={index}
                                     url={value.url}
                                     text={value.text}

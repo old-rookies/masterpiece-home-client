2b02f57a5803c02bb931471fbd83387d4696df96
seonghyeon <tjdgusdkf@gmail.com>
2019-07-16 20:15:58 +0900

    멤버, 연혁

--- src/Team/team_member/member_info.js
+++ src/Team/team_member/member_info.js
@@ -3,22 +3,22 @@ import React from 'react';
 class Member_info extends React.Component{
 
     static defaultProps = {
-        member_profile_image_url: "./member_profile/bonobono.jpg",
-        member_name: "이름",
-        member_position: "포지션",
-        member_description_text: "본인 설명 텍스트"
+        image_url: "./member_profile/bonobono.jpg",
+        name: "이름",
+        position: "포지션",
+        text: "본인 설명 텍스트"
     };
 
     render(){
         return (
         <div class="member_info_box">
-            <img src={this.props.member_profile_image_url} alt="profile_image" class="member_profile_img" />
+            <img src={this.props.image_url} alt="profile_image" class="member_profile_img" />
             <ul>
                 <li>
-                    <sapn class="member_name">{this.props.member_name}</sapn><sapn>{this.props.member_position}</sapn>
+                    <sapn class="member_name">{this.props.name}</sapn><sapn>{this.props.position}</sapn>
                 </li>
                 <li>
-                    {this.props.member_description_text}
+                    {this.props.text}
                 </li>
             </ul>
         </div>

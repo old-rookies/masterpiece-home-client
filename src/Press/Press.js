2ab1a5cdfadb3c2261db19ea6a81a59ef1d1489a
Yoo KyeongSoo <dev.yoogomja@gmail.com>
2019-08-13 14:58:46 +0900

    press list added
    Signed-off-by: Yoo KyeongSoo <dev.yoogomja@gmail.com>

--- src/Press/Press.js
+++ src/Press/Press.js
@@ -7,15 +7,17 @@ class Press extends React.Component{
     render(){
         let list = [
             { title : '기사 제목 1' , date : new Date() , press_nm : '신문사1'},
-            { title : '기사 제목 1' , date : new Date() , press_nm : '신문사1'},
-            { title : '기사 제목 1' , date : new Date() , press_nm : '신문사1'},
+            { title : '기사 제목 2' , date : new Date() , press_nm : '신문사2'},
+            { title : '기사 제목 3' , date : new Date() , press_nm : '신문사3'},
+            { title : '기사 제목 4' , date : new Date() , press_nm : '신문사4'},
+            { title : '기사 제목 5' , date : new Date() , press_nm : '신문사5'},
         ]
 
         return (
             <div>
                 <Press_title/>
                 <Press_slider/>               
-                <Press_gisa/>
+                <Press_gisa items={ list } />
           </div>
 
         );
